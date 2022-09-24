1203d5dae8afe9465e5ea33ef78c6af3cb137a11
Rio Hascaryo Winarno <riowinarno01@gmail.com>
2022-09-24 13:59:46 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis totam fugit deleniti. Sit, explicabo minima? Obcaecati error quam ut optio voluptatum aliquam quo, sint eius veritatis saepe deleniti repellat voluptate?</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
